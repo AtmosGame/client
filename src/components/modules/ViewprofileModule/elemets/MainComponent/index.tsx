c77ec348c26c51e35b0b72118ffa25cb185a6dea
Rifqi Farel Muhammad <rifqi.farel@ui.ac.id>
2023-05-15 19:10:09 +0700

    Change color schema and adding bg color for all pages

--- src/components/modules/ViewprofileModule/elemets/MainComponent/index.tsx
+++ src/components/modules/ViewprofileModule/elemets/MainComponent/index.tsx
@@ -6,19 +6,19 @@ export const MainComponent: React.FC<UserProps> = ({ user }) => (
   <div className="w-full h-full flex flex-row items-center">
     <div>
       {user?.profilePicture == '' ? (
-        <HiUserCircle className="w-16 md:w-20 h-16 md:h-20 rounded-full text-green-600" />
+        <HiUserCircle className="w-16 md:w-20 h-16 md:h-20 rounded-full text-emerald-400" />
       ) : (
         <></>
       )}
     </div>
 
     <div className="w-full pl-4 flex flex-col">
-      <h1 className="text-2xl md:text-3xl font-bold text-white">
+      <h1 className="text-2xl md:text-3xl font-bold text-emerald-400">
         {user?.username}
       </h1>
 
       <article className="pt-1">
-        <p className="text-[#ffeffe] text-sm md:text-base">{user?.bio}</p>
+        <p className="text-gray-300 text-sm md:text-base">{user?.bio}</p>
       </article>
     </div>
   </div>
