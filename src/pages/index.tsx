347e4de0daf5ea5856d122799dc5e437e9007444
eugeniusms <eugeniusms@gmail.com>
2023-05-20 22:53:43 +0700

    feat: add authbutton, logout done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { LogoutButton } from 'src/components/elements/LogoutButton'
+import { AuthButton } from 'src/components/elements/AuthButton'
 
 export default function Home() {
   return (
     <>
       <h1 className="text-white">Atmos</h1>
-      <LogoutButton />
+      <AuthButton />
     </>
   )
 }
