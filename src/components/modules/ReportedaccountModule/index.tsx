95039612df3e1b677bb8823c864decfd0e0e53a8
Abdul Rafi <abdulrafi907@gmail.com>
2023-05-25 11:28:07 +0700

    fix conflict

--- src/components/modules/ReportedaccountModule/index.tsx
+++ src/components/modules/ReportedaccountModule/index.tsx
@@ -34,44 +34,30 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
       router.push('/')
     } else if (user?.role === 'ADMIN') {
       axios
-        .get(`/api/report/${username}`, {
+      .get(`/api/report/${username}`,
+        {
           headers: {
-            Authorization: `Bearer ${Cookies.get('token')}}`,
-          },
+            Authorization: `Bearer ${Cookies.get('token')}}`
+          }
         })
-        .then(function (response) {
-          const { username, totalReports, listReports } = response.data
-          setReportedAccount({
-            username: username,
-            totalReports: totalReports,
-            listReports: listReports,
-          })
+      .then(function (response) {
+        const { username, totalReports, listReports } = response.data
+        setReportedAccount({
+          username: username,
+          totalReports: totalReports,
+          listReports: listReports,
         })
-        .catch(function (error) {
-          if (
-            error.response != undefined &&
-            error.response.status != undefined
-          ) {
-            if (
-              error.response.status === 400 ||
-              error.response.status === 404
-            ) {
-              toast({
-                title: `${error.response.data.responseMessage}`,
-                status: 'error',
-                position: 'top',
-                duration: 4000,
-                isClosable: true,
-              })
-            } else {
-              toast({
-                title: 'Terjadi kesalahan! Segera hubungi Contact Person',
-                status: 'error',
-                position: 'top',
-                duration: 4000,
-                isClosable: true,
-              })
-            }
+      })
+      .catch(function (error) {
+        if (error.response != undefined && error.response.status != undefined) {
+          if (error.response.status === 400 || error.response.status === 404) {
+            toast({
+              title: `${error.response.data.responseMessage}`,
+              status: 'error',
+              position: 'top',
+              duration: 4000,
+              isClosable: true,
+            })
           } else {
             toast({
               title: 'Terjadi kesalahan! Mohon ulangi beberapa saat lagi',
@@ -91,8 +77,8 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
           })
         }
 
-          router.push('/allreportedaccount')
-        })
+        router.push('/allreportedaccount')
+      })
     } else {
       // do nothing
     }
@@ -100,10 +86,11 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
 
   const handleApproveButton = () => {
     axios
-      .delete(`/api/report/approve/${username}`, {
+      .delete(`/api/report/approve/${username}`,
+      {
         headers: {
-          Authorization: `Bearer ${Cookies.get('token')}}`,
-        },
+          Authorization: `Bearer ${Cookies.get('token')}}`
+        }
       })
       .then(function (response) {
         toast({
@@ -150,11 +137,12 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
 
   const handleRejectButton = (id: number) => {
     axios
-      .delete(`/api/report/reject/${username}/${id}`, {
-        headers: {
-          Authorization: `Bearer ${Cookies.get('token')}}`,
-        },
-      })
+      .delete(`/api/report/reject/${username}/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get('token')}}`
+          }
+        })
       .then(function (response) {
         if (response.data.haveReport) {
           setIsUpdated(!isUpdated)
@@ -214,36 +202,30 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
     year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit',
+    second: '2-digit'
   }
 
-  function formateDate(dateReport: string) {
-    try {
+  function formateDate(dateReport: string){
+    try{
       return new Intl.DateTimeFormat('id-ID', optionsDate).format(
         new Date(dateReport)
       )
-    } catch (error) {
+    }catch (error) {
       // do nothing
     }
   }
 
   return (
     <>
-      {user?.role === 'ADMIN' ? (
+      {user?.role === 'ADMIN'? (
         <>
           <div className="flex flex-col items-center pt-5 gap-4 w-full">
-            <div className="pb-2">
-              <Button
-                variant="solid"
-                className="bg-fuchsia-700 text-white"
-                onClick={handleReportedAccountButton}
-              >
-                List Reported Account
-              </Button>
+            <div className='pb-2'>
+              <Button variant='solid' className='bg-fuchsia-700 text-white' onClick={handleReportedAccountButton}>List Reported Account</Button>
             </div>
             <h1 className="font-bold text-2xl md:text-3xl text-white text-center">{`Detail Laporan Akun ${reportedaccount?.username}`}</h1>
             <h2 className="font-bold text-lg md:text-xl text-fuchsia-500">{`Jumlah Laporan: ${reportedaccount?.totalReports}`}</h2>
-
+      
             <div className="flex flex-col justify-center items-center gap-5 w-full px-5 md:px-0">
               {reportedaccount?.listReports.map((report, key) => (
                 <div
@@ -254,27 +236,21 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
                   <span className="font-bold text-emerald-300/90 text-sm">
                     {`${formateDate(report.dateReport)}`}
                   </span>
-                  <p className="break-all font-medium text-white text-lg">
-                    {report.information}
-                  </p>
-
+                  <p className="break-all font-medium text-white text-lg">{report.information}</p>
+      
                   <div className="flex flex-row w-2/3 gap-4">
                     <button
                       className="w-1/2 bg-green-400 rounded-[10px] py-1 items-center justify-center flex"
                       onClick={() => handleApproveButton()}
                     >
-                      <span className="font-bold text-sm text-gray-750">
-                        Approve
-                      </span>
+                      <span className="font-bold text-sm text-gray-750">Approve</span>
                     </button>
-
+      
                     <button
                       className="w-1/2 bg-red-500/75 rounded-[10px] py-1 items-center justify-center flex"
                       onClick={() => handleRejectButton(report.id)}
                     >
-                      <span className="font-bold text-sm text-gray-50">
-                        Reject
-                      </span>
+                      <span className="font-bold text-sm text-gray-50">Reject</span>
                     </button>
                   </div>
                 </div>
@@ -282,7 +258,7 @@ export const ReportedaccountModule: React.FC<ParamProps> = ({ username }) => {
             </div>
           </div>
         </>
-      ) : (
+      ): (
         <></>
       )}
     </>
